7d7785f7c77325a1d4b00b03c0edd290470f93d7
manishyadavv <manishyadav.nd@gmail.com>
2017-02-20 00:23:25 +0530

    some basic changes made

--- main.rb
+++ main.rb
@@ -3,3 +3,15 @@ require 'watir'
     browser = Watir::Browser.new(:chrome)
 
     browser.goto("https://web.whatsapp.com/")
+    sleep 10
+    # type your friend's name here
+  browser.text_field(title: 'Search or start new chat').set 'NAME OF FRIEND/GROUP'
+
+
+  browser.send_keys :enter
+  element=browser.div(:class => "input")
+  10.times do
+    element.send_keys [:control, "v"]
+    browser.send_keys :enter
+    sleep 2
+  end
